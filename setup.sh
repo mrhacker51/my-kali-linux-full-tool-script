63d1007c7226b06a94f163ead620defaa99eef14
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-08-04 12:04:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,18 +18,6 @@ apt-get install dsniff -y
 echo "
 Dsniff Done +
 ";
-apt-get install python-setuptools -y
-echo "
-Python Setup 1 Done +
-";
-apt-get install python3-setuptools -y
-echo "
-Python Setup 2 Done +
-";
-apt-get install python-pip -y
-echo "
-Python Pip 1 Done +
-";
 apt-get install exiftool -y
 echo "
 Exiftool Done +
