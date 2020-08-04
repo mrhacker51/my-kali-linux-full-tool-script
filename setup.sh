80a102682bafbf65efc95371443770d932ee9237
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-08-04 08:04:37 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -133,3 +133,23 @@ gdb Download ++ gef tool
 apt-get install gdb
 wget -O ~/.gdbinit-gef.py -q https://tinyurl.com/gef-master
 echo source ~/.gdbinit-gef.py >> ~/.gdbinit
+
+
+echo "
+NetworkMiner >> Network Analyz
+";
+sudo apt-get install libmono-system-windows-forms4.0-cil
+sudo apt-get install libmono-system-web4.0-cil
+wget www.netresec.com/?download=NetworkMiner -O /tmp/nm.zip
+sudo unzip /tmp/nm.zip -d /opt/
+cd /opt/NetworkMiner*
+sudo chmod +x NetworkMiner.exe
+sudo chmod -R go+w AssembledFiles/
+sudo chmod -R go+w Captures/
+
+cat > /usr/bin/networkminer <<EOF
+#!/bin/bash
+mono /opt/NetworkMiner_2-1-1/NetworkMiner.exe
+EOF
+
+chmod +x /usr/bin/networkminer
