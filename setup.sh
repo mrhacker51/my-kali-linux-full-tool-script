982cf4f9e79a0f674f2b0452b2eb0ce28362577a
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-08-04 06:10:34 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -65,7 +65,18 @@ gem install zsteg -y
 echo "
 Zsteg Done +
 ";
-
+echo "
+Sublime Text Started +
+";
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+echo "
+Sublime Text Done +
+";
 echo "
 +++ Started +++
 +++ Downloads Folder +++ 
